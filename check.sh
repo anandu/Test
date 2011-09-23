bef8681820ab8f4de5f7fd136bf163e99761fe4b
Anand Ubale <anandu@gmail.com>
2011-09-23 22:20:40 +0530

    removed harded value

--- check.sh
+++ check.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
+# Setting the vairables
+var0=unset
 var1=unset
 var2=0
+echo "enter value to reach"
+read var0
 echo "Input variable #1 (end to exit) "
 read var1
 while
-     [ "$var1" != 120 ] # Keeps track of what $var1 was previously.
+     [ "$var1" != "$var0" ] # Keeps track of what $var1 was previously.
       # Four conditions on "while", but only last one controls loop.
       # The *last* exit status is the one that counts.
 do
  var2=`expr $var2 + 1`
  var1=`expr $var1 + $var2`
  echo "variable #1 increasing to $var1"
-if [ "$var1" != 120 ]
+if [ "$var1" != "$var0" ]
 then
  var2=`expr $var2 + 1`
  var1=`expr $var1 - $var2`
